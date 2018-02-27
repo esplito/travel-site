74cc7100f224fdb2a11f9007bbf5bdf186514bfc
esplito <eesplund@gmail.com>
2018-02-27 16:50:54 +0100

    Optimized and organized gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,8 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
-cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-  console.log("Hooray - you created a Gulp task.");
-});
 
-gulp.task('html', function() {
-  console.log("Imagine something useful being done to your HTML here.");
-});
 
-gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', function() {
 
-  watch('./app/index.html', function() {
-    gulp.start('html');
-  });
 
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
-  });
-
-});
