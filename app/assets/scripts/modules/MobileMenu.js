e76462e4c7e125fd225d6fa036d9dc45aa125112
esplito <eesplund@gmail.com>
2018-03-03 09:10:46 +0100

    Completed desktop and mobile header styling

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 class MobileMenu {
 	constructor() {
+		this.siteHeader = $(".site-header");
 		this.menuIcon = $(".site-header__menu-icon");
 		this.menuContent = $(".site-header__menu-content");
 		this.events();
@@ -13,6 +14,8 @@ class MobileMenu {
 
 	toggleTheMenu () {
 		this.menuContent.toggleClass("site-header__menu-content--is-visible");
+		this.siteHeader.toggleClass("site-header--is-expanded");
+		this.menuIcon.toggleClass("site-header__menu-icon--close-x");
 	}
 }
 
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -103,6 +103,7 @@ var MobileMenu = function () {
 	function MobileMenu() {
 		_classCallCheck(this, MobileMenu);
 
+		this.siteHeader = (0, _jquery2.default)(".site-header");
 		this.menuIcon = (0, _jquery2.default)(".site-header__menu-icon");
 		this.menuContent = (0, _jquery2.default)(".site-header__menu-content");
 		this.events();
@@ -117,6 +118,8 @@ var MobileMenu = function () {
 		key: "toggleTheMenu",
 		value: function toggleTheMenu() {
 			this.menuContent.toggleClass("site-header__menu-content--is-visible");
+			this.siteHeader.toggleClass("site-header--is-expanded");
+			this.menuIcon.toggleClass("site-header__menu-icon--close-x");
 		}
 	}]);
 
